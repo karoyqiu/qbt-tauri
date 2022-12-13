ca09ec913c5198724d45df368600fe69eba1dbbf
Roy QIU <karoyqiu@gmail.com>
2022-12-13 16:47:55 +0800

    添加不知道能不能用的 API。

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,15 +3,64 @@
     windows_subsystem = "windows"
 )]
 
+use qbittorrent_web_api::Api;
+use serde::ser::{Serialize, Serializer};
+use tauri::State;
+use tokio::sync::RwLock;
+
+struct GlobalState {
+    api: RwLock<Option<qbittorrent_web_api::api_impl::Authenticated>>,
+}
+
+#[derive(Debug, thiserror::Error)]
+enum Error {
+    #[error(transparent)]
+    Api(#[from] qbittorrent_web_api::api_impl::Error),
+}
+
+impl Serialize for Error {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(&self.to_string())
+    }
+}
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+async fn qbt_login(
+    base_url: &str,
+    username: &str,
+    password: &str,
+    state: State<'_, GlobalState>,
+) -> Result<(), Error> {
+    let a = Api::login(base_url, username, password).await?;
+    let mut api = state.api.write().await;
+    *api = Some(a);
+    Ok(())
+}
+
+#[tauri::command]
+async fn qbt_get_torrents_info(
+    filter: &str,
+    state: State<'_, GlobalState>,
+) -> Result<Vec<qbittorrent_web_api::api_impl::torrent_management::info::Response>, Error> {
+    let api = state.api.read().await;
+    let api = api.as_ref();
+    let api = api.expect("no-api");
+    let tm = api.torrent_management();
+    let builder = tm.info().filter(filter);
+
+    Ok(builder.send().await?)
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .manage(GlobalState {
+            api: Default::default(),
+        })
+        .invoke_handler(tauri::generate_handler![qbt_login, qbt_get_torrents_info])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
